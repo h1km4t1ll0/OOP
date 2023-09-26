9a236d2dfc5003c652cc6e87ce8b09073c8365cd
hikmatillo <dolgovdaniil007@gmail.com>
2023-09-26 20:44:59 +0700

    fix: add tests for Main class

--- Task_1_1_1/src/test/java/ru/nsu/dolgov/main/MainTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/dolgov/main/MainTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 public class MainTest {
     @Test
     void testMainClass() {
+        String[] testArguments = new String[] {"tetsArg"};
+        Main.main(testArguments);
         assert(true);
     }
 }
