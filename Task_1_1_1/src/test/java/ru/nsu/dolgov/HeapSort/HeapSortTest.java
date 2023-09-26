49bdcf4a0127b007fe312d2b2ae3d5d3a231928d
hikmatillo <dolgovdaniil007@gmail.com>
2023-09-26 20:54:03 +0700

    fix: check style

--- Task_1_1_1/src/test/java/ru/nsu/dolgov/HeapSort/HeapSortTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/dolgov/HeapSort/HeapSortTest.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 
 import java.util.Random;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class HeapSortTest {
     @Test
