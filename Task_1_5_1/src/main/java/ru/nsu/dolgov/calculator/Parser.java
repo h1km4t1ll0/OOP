5c37917e53a0ee8698e50ff55e9d98ac8620d15f
hikmatillo <dolgovdaniil007@gmail.com>
2023-12-06 00:05:11 +0700

    add: javadoc for Parser class

--- Task_1_5_1/src/main/java/ru/nsu/dolgov/calculator/Parser.java
+++ Task_1_5_1/src/main/java/ru/nsu/dolgov/calculator/Parser.java
@@ -1,6 +1,5 @@
 package ru.nsu.dolgov.calculator;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -21,11 +20,19 @@ public class Parser {
         this.stringExpression = stringExpression;
     }
 
+    /**
+     * Parses string to Expression.
+     * @return Expression.
+     */
     public Expression parse() {
         String[] tokenizedStringExpression = this.stringExpression.split("\\s+");
         return new Expression(tokenizedStringExpression);
     }
 
+    /**
+     * Checks if input could be parsed or not.
+     * @return true if could else otherwise.
+     */
     public boolean check() {
         return Arrays.stream(this.stringExpression.split("\\s+")).allMatch(
                 availableTokens::contains
