eb9409cbdd5e43397247fe8f75f6a1a358d164e0
hikmatillo <dolgovdaniil007@gmail.com>
2023-12-05 22:49:12 +0700

    add: initial commit

---
+++ Task_1_5_1/src/main/java/ru/nsu/dolgov/calculator/Main.java
@@ -0,0 +1,8 @@
+package ru.nsu.dolgov.calculator;
+
+/**
+ * An entrypoint for a calculator.
+ */
+public class Main {
+
+}
---
+++ Task_1_5_1/src/test/java/ru/nsu/dolgov/calculator/CalculatorTest.java
@@ -0,0 +1,15 @@
+package ru.nsu.dolgov.calculator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Class for Calculator class test.
+ */
+public class CalculatorTest {
+}
