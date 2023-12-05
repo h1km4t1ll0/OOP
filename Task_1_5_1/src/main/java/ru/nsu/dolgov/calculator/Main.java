b8fa7d9f9fc22b0d670f6fd804f2dc0e4e0e9206
hikmatillo <dolgovdaniil007@gmail.com>
2023-12-05 23:46:53 +0700

    add: Main class implementation

--- Task_1_5_1/src/main/java/ru/nsu/dolgov/calculator/Main.java
+++ Task_1_5_1/src/main/java/ru/nsu/dolgov/calculator/Main.java
@@ -1,8 +1,28 @@
 package ru.nsu.dolgov.calculator;
 
+import java.util.Scanner;
+
 /**
  * An entrypoint for a calculator.
  */
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Enter the expression:");
+        System.out.println("Output: " + calculate());
+    }
 
+    private static double calculate() {
+        Expression expression = parseInput();
+        Calculator calculator = new Calculator(expression);
+        return calculator.calculate();
+    }
+
+    private static Expression parseInput() {
+        Scanner scanner = new Scanner(System.in);
+        String inputString = scanner.nextLine();
+        scanner.close();
+        Parser parser = new Parser(inputString);
+
+        return parser.parse();
+    }
 }
