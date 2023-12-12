a37ba5a338645fd57c1841bc9ef1b6c2200af85a
hikmatillo <dolgovdaniil007@gmail.com>
2023-12-12 23:04:45 +0700

    add: class description add: API to access deque add: isOperand method fix: fields are private

--- Task_1_5_1/src/main/java/ru/nsu/dolgov/calculator/Expression.java
+++ Task_1_5_1/src/main/java/ru/nsu/dolgov/calculator/Expression.java
@@ -5,10 +5,11 @@ import java.util.LinkedList;
 
 /**
  * Class for expression implementation.
+ * Its purpose is to store the deque and provide an API to access it
  */
 public class Expression {
-    public final Deque<Double> stack;
-    public String[] rawTokens;
+    private final Deque<Double> stack;
+    private final String[] rawTokens;
 
     public Expression(String[] parsedExpression) {
         this.stack = new LinkedList<>();
@@ -28,5 +29,48 @@ public class Expression {
         } catch (NumberFormatException e) {
             return false;
         }
+    }
+
+    /**
+     * Method to check whether token is operand or not.
+     *
+     * @param token string token.
+     * @return true if operand false otherwise.
+     */
+    public static boolean isOperand(String token) {
+        try {
+            Operations.getOperation(token);
+            return true;
+        } catch (IndexOutOfBoundsException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Getter for the tokens collected from user.
+     *
+     * @return String[] tokens.
+     */
+    public String[] getRawTokens() {
+        return this.rawTokens;
+    }
+
+    /**
+     * Method to push the data to an internal deque.
+     *
+     * @param expression expression to be pushed.
+     */
+    public void push(Double expression) {
+        this.stack.push(expression);
+    }
+
+
+    /**
+     * Method to get the data from the internal deque.
+     *
+     * @return Double value.
+     */
+    public Double pop() {
+        return this.stack.pop();
     }
 }
