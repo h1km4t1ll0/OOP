a0d3bf504390fe1e6d5364a8486b2833226b7c5f
h1km4t1ll0 <h1km4t1ll0@users.noreply.github.com>
2023-12-12 16:18:48 +0000

    Deploying to gh-pages from @ h1km4t1ll0/OOP@dcb41ae2c6f9cc0b954445fc8e6ae049bc25a34d 🚀

--- Task_1_5_1/type-search-index.js
+++ Task_1_5_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.dolgov.calculator","l":"Calculator"},{"p":"ru.nsu.dolgov.calculator","l":"Expression"},{"p":"ru.nsu.dolgov.calculator","l":"Main"},{"p":"ru.nsu.dolgov.calculator","l":"Parser"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.dolgov.calculator","l":"Calculator"},{"p":"ru.nsu.dolgov.calculator","l":"Expression"},{"p":"ru.nsu.dolgov.calculator","l":"Main"},{"p":"ru.nsu.dolgov.calculator","l":"Operations"},{"p":"ru.nsu.dolgov.calculator","l":"Parser"}]
