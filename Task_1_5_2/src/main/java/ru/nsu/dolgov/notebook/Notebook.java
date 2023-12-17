d2972ef1c49c4061903cf4486c4da7443e1aa0e0
hikmatillo <dolgovdaniil007@gmail.com>
2023-12-17 17:18:34 +0700

    add: javadoc for Notebook class

--- Task_1_5_2/src/main/java/ru/nsu/dolgov/notebook/Notebook.java
+++ Task_1_5_2/src/main/java/ru/nsu/dolgov/notebook/Notebook.java
@@ -4,15 +4,29 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+/**
+ * Class for notebook implementation
+ */
 public class Notebook {
     private final Map<String, Note> notes = new HashMap<>();
 
+    /**
+     * Method to add a note.
+     *
+     * @param summary summary of a note.
+     * @param content content of a note.
+     */
     public void addNote(String summary, String content) {
         Note note = new Note(summary, content);
         notes.put(note.getId(), note);
 
     }
 
+    /**
+     * Method to remove a note.
+     *
+     * @param id id of a note to delete.
+     */
     public void removeNote(String id) {
         notes.remove(id);
     }
