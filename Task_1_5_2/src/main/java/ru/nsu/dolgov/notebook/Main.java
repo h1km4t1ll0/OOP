cd0920a2a8740814ce344a99dac4a77fd7df8795
hikmatillo <dolgovdaniil007@gmail.com>
2023-12-17 16:25:21 +0700

    add: draft Main class

--- Task_1_5_2/src/main/java/ru/nsu/dolgov/notebook/Main.java
+++ Task_1_5_2/src/main/java/ru/nsu/dolgov/notebook/Main.java
@@ -1,11 +1,33 @@
 package ru.nsu.dolgov.notebook;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
 import java.util.Scanner;
 
 /**
  * An entrypoint for a notebook.
  */
 public class Main {
+    /**
+     * Optional arguments for operations with the notebook.
+     */
+    @Option(name = "-add", usage = "Command to add an element: -add [key] [value]")
+    private boolean addCommand;
+
+    @Option(name = "-show", usage = "Command to show all: -show")
+    private boolean showCommand;
+
+    @Option(name = "-rm", usage = "Command to delete an element: -rm [key]")
+    private boolean rmCommand;
+
+    @Argument
+    private List<String> arguments = new ArrayList<>();
+
     /**
      * An entrypoint.
      */
