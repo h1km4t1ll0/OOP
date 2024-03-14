e2a51c1f8fb97cf94e3da3233e35c9f463de50a3
hikmatillo <dolgovdaniil007@gmail.com>
2024-03-14 21:41:24 +0700

    add: improve order state

---
+++ Task_2_2_1/src/main/java/ru/nsu/dolgov/pizzeria/service/entities/JSONEntites/Orders.java
@@ -0,0 +1,9 @@
+package ru.nsu.dolgov.pizzeria.service.entities.JSONEntites;
+
+import ru.nsu.dolgov.pizzeria.service.entities.pureentities.Order;
+
+import java.util.List;
+
+public class Orders {
+    public int quantity;
+}
--- Task_2_2_1/src/main/java/ru/nsu/dolgov/pizzeria/service/entities/pureentities/OrderState.java
+++ Task_2_2_1/src/main/java/ru/nsu/dolgov/pizzeria/service/entities/pureentities/OrderState.java
@@ -1,5 +1,5 @@
 package ru.nsu.dolgov.pizzeria.service.entities.pureentities;
 
 public enum OrderState {
-    WAITING, PREPARING, DELIVERING, DONE
+    WAITING, PREPARING, WAREHOUSE, DELIVERING, DONE
 }
