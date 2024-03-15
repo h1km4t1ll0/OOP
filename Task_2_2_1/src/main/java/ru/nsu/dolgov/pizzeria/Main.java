b4d105087b11e25ccddd3474bfb0da31a6c482c5
hikmatillo <dolgovdaniil007@gmail.com>
2024-03-15 11:50:36 +0700

    fix: remove unused imports

--- Task_2_2_1/src/main/java/ru/nsu/dolgov/pizzeria/Main.java
+++ Task_2_2_1/src/main/java/ru/nsu/dolgov/pizzeria/Main.java
@@ -1,20 +1,12 @@
 package ru.nsu.dolgov.pizzeria;
 
-import org.jfree.chart.plot.ThermometerPlot;
-import ru.nsu.dolgov.pizzeria.service.entities.pureentities.Baker;
-import ru.nsu.dolgov.pizzeria.service.entities.pureentities.Deliverer;
-import ru.nsu.dolgov.pizzeria.service.entities.pureentities.Order;
 import ru.nsu.dolgov.pizzeria.service.entities.pureentities.Runner;
 import ru.nsu.dolgov.pizzeria.service.init.Init;
-import ru.nsu.dolgov.pizzeria.service.queues.BaseQueue;
 
-import javax.annotation.processing.SupportedSourceVersion;
-import java.util.*;
-import java.util.concurrent.*;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        Init initializer = new Init("config.json");
+        Init initializer = new Init("config.json", "dump.json");
         Runner runner = new Runner(
             initializer,
             initializer.getConfiguration().bakery.durationOfTheDayInSeconds * 1000L
